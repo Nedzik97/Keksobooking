5f04427cb1d95b64b1313cc27de266ade4d8eee4
Nedzik <113420019+Nedzik97@users.noreply.github.com>
2022-12-05 10:06:51 +0200

    fixed bugs in js/main.js

--- js/main.js
+++ js/main.js
@@ -12,26 +12,31 @@ function getRandomNumberFloatingPoint(minNumber, maxNumber, floatingPoint) {
   const lower = Math.min(Math.abs(minNumber), Math.abs(maxNumber));
   const upper = Math.max(Math.abs(minNumber), Math.abs(maxNumber));
   const  randomNumberFloat = (Math.random() * (upper - lower) + lower);
-  return randomNumberFloat.toFixed(floatingPoint);
+  const randomDigitFloat = randomNumberFloat.toFixed(floatingPoint);
+  return randomDigitFloat;
 }
 
 getRandomNumberFloatingPoint(140, 100, 1);
 
 
-const typeHousing = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const timeCheckin = ['12:00', '13:00', '14:00'];
-const timeCheckout = ['12:00', '13:00', '14:00'];
-const featuresHousing = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.'];
+const houseTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkinTimes = ['12:00', '13:00', '14:00'];
+const checkoutTimes = ['12:00', '13:00', '14:00'];
+const houseFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.'
+];
 
-const getArrayLength = () => {
-  const maxLength = featuresHousing.length;
+const getArrayLength = (array) => {
+  const mainArray = array;
+  const maxLength = mainArray.length;
   const lengthOfArray = getRandomInteger(1, maxLength);
   const randomArray = [];
 
   for(let i = 0; i < lengthOfArray; i++) {
     const indexOfEl = getRandomInteger(0, 5);
-    const el = featuresHousing[indexOfEl];
+    const el = mainArray[indexOfEl];
 
     if (!randomArray.includes(el)) {
       randomArray.push(el);
@@ -40,30 +45,10 @@ const getArrayLength = () => {
   return randomArray;
 };
 
-getArrayLength();
-
-const getPhotosLength = () => {
-  const maxLength = photos.length;
-  const lengthOfPhotos = getRandomInteger(1, maxLength);
-  const randomPhotos = [];
-
-  for(let i = 0; i < lengthOfPhotos; i++) {
-    const indexOfEl = getRandomInteger(0, 2);
-    const el = photos[indexOfEl];
-
-    if (!randomPhotos.includes(el)) {
-      randomPhotos.push(el);
-    }
-  }
-  return randomPhotos;
-};
-
-getPhotosLength();
-
 
 const createRandomAuthor = () => {
   const author = {
-    avatar: `img/avatars/user${  getRandomInteger(1, 10)  }.png`
+    avatar: `img/avatars/user${getRandomInteger(1, 10)}.png`
   };
   return author;
 };
@@ -87,13 +72,13 @@ const createRandomOffer = () => {
     title: '«Любите захватывающий вид? Райский шалаш для молодоженов. Исключительный случай – дом в нашем районе со скидкой!»',
     adress: createRandomLocation(),
     price: getRandomInteger(1000, 15000),
-    rooms: typeHousing[getRandomInteger(0, 4)],
+    rooms: houseTypes[getRandomInteger(0, 4)],
     guests: getRandomInteger(1, 20),
-    checkin: timeCheckin[getRandomInteger(0, 2)],
-    checkout: timeCheckout[getRandomInteger(0, 2)],
-    features: getArrayLength(),
+    checkin: checkinTimes[getRandomInteger(0, 2)],
+    checkout: checkoutTimes[getRandomInteger(0, 2)],
+    features: getArrayLength(houseFeatures),
     description: 'У меня небольшая светлая комната с приятными светло-серыми обоями. На всю стену — огромное окно, через которое к комнате утром заглядывает солнышко.',
-    photos: getPhotosLength()
+    photos: getArrayLength(photos)
   };
   return offer;
 };
@@ -107,6 +92,7 @@ const getHouseRental = () => ({
   location: createRandomLocation()
 });
 
-const getHouseRentalCount =  Array.from({length: 10}, getHouseRental);
+const QUANTITYENTITY = 10;
 
+const getHouseRentalCount =  Array.from({length: QUANTITYENTITY}, getHouseRental);
 
