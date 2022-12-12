555a3e314fc27720f55246462233bda87d300747
Nedzik <113420019+Nedzik97@users.noreply.github.com>
2022-12-12 12:20:03 +0200

    fixed bugs and devided into modules

--- js/random-object.js
+++ js/random-object.js
@@ -1,65 +1,80 @@
-import {getRandomInteger, getRandomNumberFloatingPoint, getArrayLength} from './until.js';
+import {getRandomInteger, getRandomNumberFloatingPoint, getRandomLength} from './util.js';
 
-
+const QUANTITYENTITY = 10;
 const houseTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const checkinTimes = ['12:00', '13:00', '14:00'];
 const checkoutTimes = ['12:00', '13:00', '14:00'];
 const houseFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+const photos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.'
+];
+const photoIndexes = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+
+const headlines = [
+  'Любите захватывающий вид? Райский шалаш для молодоженов. Исключительный случай – дом в нашем районе со скидкой!',
+  'Увлекательное место для отдыха с друзьями',
+  'Устали от шума и городской суеты? Тогда это место для вас',
+  'Этот дом возле озера может стать вашим на этот уикенд',
+  'Живите красиво уже сегодня, подчеркните свой статус, квартира в самом центре города для вас!'
+];
+
+const houseDescriptions = [
+  'Чудесный дом с видом на море, а также собственный пляж!',
+  'У меня небольшая светлая комната с приятными светло-серыми обоями. На всю стену — огромное окно, через которое к комнате утром заглядывает солнышко.',
+  'Квартира с большой гостинной комнатой в которой вам точно будет комфортно',
+  'Уютный домик в лесу для семьи. На территории есть все удобства для отдыха',
+  'Небольшая квартира-студия с личным кабинетом и панорамными окнами из которых открывается великолепный вид на Манхэттен'
 ];
 
 
 const createRandomAuthor = () => {
   const author = {
-    avatar: `img/avatars/user${getRandomInteger(1, 10)}.png`
+    avatar: `img/avatars/user${photoIndexes.shift()}.png`
   };
   return author;
 };
 
-createRandomAuthor();
-
 
 const createRandomLocation = () => {
   const location = {
-    lat: (getRandomNumberFloatingPoint(35.65, 35.70, 5)),
-    lng: (getRandomNumberFloatingPoint(139.7, 139.8, 5))
+    lat: getRandomNumberFloatingPoint(35.65, 35.70, 5),
+    lng: getRandomNumberFloatingPoint(139.7, 139.8, 5)
   };
   return location;
 };
 
-createRandomLocation();
 
-
-const createRandomOffer = () => {
+const createRandomOffer = (location) => {
   const offer = {
-    title: '«Любите захватывающий вид? Райский шалаш для молодоженов. Исключительный случай – дом в нашем районе со скидкой!»',
-    adress: createRandomLocation(),
+    title: headlines[getRandomInteger(0, 4)],
+    adress: location,
     price: getRandomInteger(1000, 15000),
-    rooms: houseTypes[getRandomInteger(0, 4)],
+    type: houseTypes[getRandomInteger(0, 4)],
+    rooms: getRandomInteger(1, 10),
     guests: getRandomInteger(1, 20),
     checkin: checkinTimes[getRandomInteger(0, 2)],
     checkout: checkoutTimes[getRandomInteger(0, 2)],
-    features: getArrayLength(houseFeatures),
-    description: 'У меня небольшая светлая комната с приятными светло-серыми обоями. На всю стену — огромное окно, через которое к комнате утром заглядывает солнышко.',
-    photos: getArrayLength(photos)
+    features: getRandomLength(houseFeatures),
+    description: houseDescriptions[getRandomInteger(0, 4)],
+    photos: getRandomLength(photos)
   };
   return offer;
 };
 
-createRandomOffer();
 
+const getHouseRental = () => {
+  const location = createRandomLocation();
 
-const getHouseRental = () => ({
-  author: createRandomAuthor(),
-  offer: createRandomOffer(),
-  location: createRandomLocation()
-});
+  return {
+    author: createRandomAuthor(),
+    offer: createRandomOffer(location),
+    location,
+  };
+};
 
-const QUANTITYENTITY = 10;
 
 const getHouseRentalCount = () => Array.from({length: QUANTITYENTITY}, getHouseRental);
-
 
 export {getHouseRentalCount};
--- js/util.js
+++ js/util.js
@@ -1,24 +1,21 @@
-function getRandomInteger(min, max) {
+const getRandomInteger = (min, max) => {
   const lower = Math.min(Math.abs(min), Math.abs(max));
   const upper =  Math.max(Math.abs(min), Math.abs(max));
   const randomNumber = (Math.random() * (upper - lower) + lower);
   return Math.floor(randomNumber);
-}
-
-getRandomInteger(20, 50);
+};
 
 
-function getRandomNumberFloatingPoint(minNumber, maxNumber, floatingPoint) {
+const getRandomNumberFloatingPoint = (minNumber, maxNumber, floatingPoint) => {
   const lower = Math.min(Math.abs(minNumber), Math.abs(maxNumber));
   const upper = Math.max(Math.abs(minNumber), Math.abs(maxNumber));
   const  randomNumberFloat = (Math.random() * (upper - lower) + lower);
   const randomDigitFloat = randomNumberFloat.toFixed(floatingPoint);
   return parseFloat(randomDigitFloat);
-}
+};
 
-(getRandomNumberFloatingPoint(140, 100, 1));
 
-const getArrayLength = (array) => {
+const getRandomLength = (array) => {
   const mainArray = array;
   const maxLength = mainArray.length;
   const lengthOfArray = getRandomInteger(1, maxLength);
@@ -35,4 +32,6 @@ const getArrayLength = (array) => {
   return randomArray;
 };
 
-export {getRandomInteger, getRandomNumberFloatingPoint, getArrayLength};
+export {getRandomInteger, getRandomNumberFloatingPoint, getRandomLength};
+
+
